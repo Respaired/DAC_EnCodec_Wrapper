622fac53675b789ff28e7ee72f247aced3e69a4b
Soshyant <97403512+Respaired@users.noreply.github.com>
2025-05-26 13:32:14 +0330

    Update configuration_dac.py

--- configuration_dac.py
+++ configuration_dac.py
@@ -1,31 +1,11 @@
-# coding=utf-8
-# Copyright 2024 Descript and The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Dac model configuration"""
 
-import math
-
-import numpy as np
-
-from ...configuration_utils import PretrainedConfig
-from ...utils import logging
+from transformers import PretrainedConfig
+from importlib.metadata import version
+from packaging.version import Version
 
 
-logger = logging.get_logger(__name__)
 
-
-class DacConfig(PretrainedConfig):
+class DACConfig(PretrainedConfig):
     r"""
     This is the configuration class to store the configuration of an [`DacModel`]. It is used to instantiate a
     Dac model according to the specified arguments, defining the model architecture. Instantiating a configuration
@@ -114,4 +94,4 @@ class DacConfig(PretrainedConfig):
         return math.ceil(self.sampling_rate / hop_length)
 
 
-__all__ = ["DacConfig"]
+__all__ = ["DACConfig"]
